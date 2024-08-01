0ff6f515d834e4c22c563eb4400f5826f6e1918d
hmxf <gouych13@icloud.com>
2024-08-01 17:35:14 +0800

    Update script to fit WSL2 running

--- scripts/bind_usb.sh
+++ scripts/bind_usb.sh
@@ -5,7 +5,7 @@ sudo cp scripts/imu_usb.rules /etc/udev/rules.d
 sudo cp scripts/gps_usb.rules /etc/udev/rules.d
 sudo chmod 777 /etc/udev/rules.d/imu_usb.rules
 sudo chmod 777 /etc/udev/rules.d/gps_usb.rules
-service udev reload
+sudo service udev reload
 sleep 2
-service udev restart
+sudo service udev restart
 echo "Finish!!!"
